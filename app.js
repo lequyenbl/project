c5396415706d9904cd51c8459df8cca7bbe3b095
lequyenbl <51352299+lequyenbl@users.noreply.github.com>
2019-06-04 11:00:43 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import * as helperModule from './my-helper-module';
+import * as _ from 'lodash';
+import './sharedModule';
+import './../styles/app.css';
+import './../styles/appStyles.scss';
+
+console.log("Welcome! Greetings from app.js. Let's learn Webpack2");
+
+console.log(helperModule.greetings);
+
+var arr=[ 1, 2, 3];
+_.each(arr,function(val) {
+ console.log('Output from Lodash _.each for Element ' + val); 
+});
